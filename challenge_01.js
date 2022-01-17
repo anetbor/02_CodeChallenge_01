9f729f64dc3909196eb391643f79f2e1749d02c6
Anet <anetborin@mail.com>
2022-01-17 12:57:40 +0100

    Summe

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,37 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+// let ersteZahl, zweiteZahl;
+//  ersteZahl =Number (prompt ("eine Zahl eingeben:"));
+//  zweiteZahl = Number (prompt ("zweite Zahl eingeben:"));
+// console.log(typeof ersteZahl);
+// console.log(typeof zweiteZahl);
+
+
+
+//  summebeidenZahlen = ersteZahl + zweiteZahl;
+
+//  console.log (summebeidenZahlen);'''
+
+
+
+//zweite Variante 
+// let zahl1, zahl2;
+
+// zahl1 = parseInt(prompt("Zahl 1:"));
+// zahl2 = parseInt(prompt("Zahl 2:"));
+// summe = zahl1 + zahl2;
+// console.log("summe ist:" + summe);
+
+
+//dritte Variante
+console.log
+(
+    "Die Summe" +
+(
+parseFloat(prompt("Zahl 1 eingeben")) +
+parseFloat(prompt("Zahl 2 eingeben"))
+)
+);
+
+
